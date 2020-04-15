9dfb7f7f9b0716dfcca96e68bce7ca51777b8a8c
Diogo Justen <diogo@deevio.ai>
2020-04-15 19:29:34 +0200

    Removed memory copy

--- src/compress.c
+++ src/compress.c
@@ -3,94 +3,86 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include "filter.h"
 #include "bit_stream.h"
 
-void printPixels(const uint8_t *pixels, int size)
+#define MAX_ZERO_SPECIAL_CODE 272
+
+void printPixels(const uint8_t *pixels, int width, int sizeTotal)
 {
-  int i;
   const uint8_t *p = pixels;
+  int lineSize = (width | 0x3) + 1;
+  int currentLine = 0;
+  int i;
 
-  for (i = 0; i < size; i++)
+  for (i = 0; i < sizeTotal;)
   {
-    printf("%x ", *p);
-    p++;
+    printf("%X ", *(p + i));
+
+    if (++i >= (currentLine * lineSize) + width)
+    {
+      i = (i | 0x3) + 1;
+      currentLine++;
+    }
   }
+
   printf("\n");
-}
-
-int CountPixel(const uint8_t *pValidPixels, int remainingPixels)
-{
-  if (remainingPixels <= 1)
-  {
-    printf("Error\n");
-    return 0;
-  }
-
-  uint8_t pixel = *pValidPixels;
-  //current pixel different from the next
-  if (pixel != *(pValidPixels + 1))
-  {
-    return 1;
-  }
-
-  /* To discuss later
-   * "...<width> has a typical value of 256 to 4096..."
-   * uint8_t blankBlock[256] = { 0 };
-   * memset(blankBlock, 0, sizeof(blankBlock);
-   * memcmp(blankBlock, pValidPixels);
-   * if equal than return 256
-   */
-
-  int i = 0;
-  int pixelCount = 1;
-
-  while ((i < (remainingPixels - 1)) && (pixel == *(pValidPixels + 1)))
-  {
-    pValidPixels++;
-    pixelCount++;
-    i++;
-  }
-
-  return pixelCount;
 }
 
 uint8_t *Compress(const uint8_t *pixels, uint_fast16_t width, uint_fast16_t height, uint32_t *size)
 {
-  uint32_t validPixelsSize = width * height;
-  uint8_t *validPixels = Filter(pixels, width, validPixelsSize);
+  int lineSize = (width | 0x3) + 1;
+  int sizeTotal = height * lineSize;
+
+  int currentPixelIndex = 0;
+  int currentLine = 0;
+  int count = 1;
 
   //Debug and to show results
   printf("Valid pixels: ");
-  printPixels(validPixels, validPixelsSize);
+  printPixels(pixels, width, sizeTotal);
 
-  const uint8_t *pValidPixels = validPixels;
-  int remainingPixels;
-  for (remainingPixels = validPixelsSize; remainingPixels > 0;)
+  uint8_t bitsUsed;
+  uint32_t bitStream;
+
+  for (int nextPixelIndex = 1; nextPixelIndex <= sizeTotal;)
   {
-    int numSamePixel = CountPixel(pValidPixels, remainingPixels);
-    if (numSamePixel <= 0)
+    const uint8_t *currentPixel = pixels + currentPixelIndex;
+
+    //last valid pixel is different from previous
+    if (nextPixelIndex >= sizeTotal)
     {
-      printf("Unable to count pixel %x\n", *pValidPixels);
-      free(validPixels);
-      return NULL;
+      BitStreamConvert(*currentPixel, count, &bitStream, &bitsUsed);
+      BitStreamPrint(bitStream, bitsUsed);
+      break;
     }
 
-    //Debug
-    //printf("Pixel: %x Samples: %dx\n", *pValidPixels, numSamePixel);
+    const uint8_t *nextPixel = pixels + nextPixelIndex;
+    if (*currentPixel == *nextPixel)
+    {
+      if (++count == MAX_ZERO_SPECIAL_CODE)
+      {
+        BitStreamConvert(*currentPixel, count, &bitStream, &bitsUsed);
+        BitStreamPrint(bitStream, bitsUsed);
+        currentPixelIndex = nextPixelIndex;
+        count = 1;
+      }
+    }
+    else
+    {
+      BitStreamConvert(*currentPixel, count, &bitStream, &bitsUsed);
+      BitStreamPrint(bitStream, bitsUsed);
+      currentPixelIndex = nextPixelIndex;
+      count = 1;
+    }
 
-    uint8_t bitsUsed;
-    uint32_t bitStream;
-
-    BitStreamConvert(*pValidPixels, numSamePixel, &bitStream, &bitsUsed);
-
-    //Debug Only
-    BitStreamPrint(bitStream, bitsUsed);
-
-    pValidPixels += numSamePixel;
-    remainingPixels -= numSamePixel;
+    if (++nextPixelIndex >= (currentLine * lineSize) + width)
+    {
+      nextPixelIndex = (nextPixelIndex | 0x3) + 1;
+      currentLine++;
+    }
   }
 
-  *size = validPixelsSize;
-  return validPixels;
+  printf("\n");
+
+  return NULL;
 }
